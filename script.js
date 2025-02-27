fd28ab236cfbf590f97067c4e47ae74cd4ceb3e7
mnosumon <mdnuruddin378350@gmail.com>
2025-02-27 12:46:43 +0600

    xmlhttprequest Properties state

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function loadData() {
   const xhttp = new XMLHttpRequest();
-  xhttp.onload = function () {
-    $(".container01").text(this.responseText); //request pathiye je result asbe
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      $(".container01").text(this.responseText); //request pathiye je result asbe
+    }
   };
   xhttp.open("GET", "text.txt"); //je jayga theke data nibo
   xhttp.send(); //request ti send korte hobe
+}
+
+//jQuery AJAX.......
+function loadData() {
+  $.get("text.txt", function (data) {
+    $(".container01").text(data); // ডাটা আসার পর সেটা দেখাবো
+  }).fail(function () {
+    console.log("Error loading data.");
+  });
 }
